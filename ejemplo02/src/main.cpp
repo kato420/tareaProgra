a51991f569197000e5ed8fbb1804fa1ee3a816f0
kato <yhlx05@gmail.com>
2025-04-03 00:35:47 -0500

    Ejemplo 02

--- ejemplo02/src/main.cpp
+++ ejemplo02/src/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
+// EJEMPLO 02
 int main(int argc, char const *argv[])
 {
-    cout << "Proyecto 02";
+    double a, b, c;
+    cin >> a >> b >> c;
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        cout << "El triangulo valido es ";
+        if (a == b && b == c && a == c)
+        {
+            cout << " equilatero." << endl;
+        }
+        else
+        {
+            if (a == b || b == c || a == c)
+            {
+                cout << " isóceles." << endl;
+            }
+            else
+            {
+                cout << " escalar." << endl;
+            }
+        }
+    }
+    else
+    {
+        cout << "No es triángulo válido." << endl;
+    }
+
     return 0;
 }

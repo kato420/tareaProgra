f361491db4fe5ac573c6eabf8f9945e4c80d61ae
kato <yhlx05@gmail.com>
2025-04-03 00:27:49 -0500

    Primer Ejemplo

--- ejemplo01/src/main.cpp
+++ ejemplo01/src/main.cpp
@@ -1,7 +1,27 @@
 #include <iostream>
 using namespace std;
-int main(int argc, char const *argv[])
+
+// Ejercicio 01
+int main(int argc, char *argv[])
 {
-    cout << "Proyecto 01";
+    int cantidad;
+    double precio;
+    cin >> cantidad;
+    cin >> precio;
+    double total = cantidad * precio;
+    if (1001 <= cantidad)
+    {
+        total *= 0.6;
+    }
+    else if (251 <= cantidad)
+    {
+        total *= 0.8;
+    }
+    else if (101 <= cantidad)
+    {
+        total *= 0.9;
+    }
+
+    cout << "El pago  por la compra es: " << total << " soles." << endl;
     return 0;
 }
